5f854dd7119cbea8bcf17d10603bf6b434ac4532
yogi2050 <yogmicro@gmail.com>
2024-04-11 16:12:43 +0530

    update version

--- version.tf
+++ version.tf
@@ -1,10 +1,5 @@
 
 terraform {
-  backend "s3" {
-    bucket = "stuffnz-terraform-state-store"
-    key    = "stuff-uat/terraform-uat.tfstate"
-    region = "ap-southeast-2"
-  }
   required_version = "1.2.3"
 
   required_providers {
